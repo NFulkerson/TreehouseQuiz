fd953c0e596280dab693ad1a6d8f2fdf1dafdd07
Nathan Fulkerson <nm.fulkerson@gmail.com>
2016-08-18 16:18:15 +0900

    Prevent user from advancing to game over by abusing multiple taps, catch instances where questions in round might exceed the limit.

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -5,6 +5,9 @@
 //  Created by Pasan Premaratne on 3/9/16.
 //  Copyright © 2016 Treehouse. All rights reserved.
 //
+// TODO: the combination of checkAnswer + delay gives players a chance to cheat or mistakenly break the game:
+// if one taps a button multiple times, they can increment the counter beyond the number of rounds,
+// leading to endless loop of questions. Or they can skip ahead to the victory screen by tapping a correct answer four times.
 
 import UIKit
 import GameKit
@@ -16,10 +19,14 @@ class ViewController: UIViewController {
     var questionsAsked = 0
     var correctQuestions = 0
     var indexOfSelectedQuestion: Int = 0
-    
+    var askedIndexes: [Int] = [] // indexes of questions asked. Check this against selected question.
+                                 // if it matches, then find another question.
+                                 // We could pop/remove questions from our Trivia.questions array, but we would need
+                                 // additional logic to repopulate the array for a new game.
     let trivia = Trivia()
     
     @IBOutlet weak var questionField: UILabel!
+    // IBOutletCollection would be good here, in order to setup and tear down various things.
     @IBOutlet weak var choiceA: UIButton!
     @IBOutlet weak var choiceB: UIButton!
     @IBOutlet weak var choiceC: UIButton!
@@ -60,6 +67,7 @@ class ViewController: UIViewController {
         var choices = question.choices
         // reset hiding of buttons.
         showAllChoices()
+        resetButtons()
         if (choices.count == 2) {
             choiceA.setTitle("True", forState: .Normal)
             choiceB.setTitle("False", forState: .Normal)
@@ -92,8 +100,10 @@ class ViewController: UIViewController {
         questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
         
     }
-    
+    // TODO: Limit touches to 1, or disable the buttons in some way to prevent breaking the game.
     @IBAction func checkAnswer(sender: UIButton) {
+        highlightAnswer()
+        
         // Increment the questions asked counter
         questionsAsked += 1
         
@@ -113,7 +123,8 @@ class ViewController: UIViewController {
     }
     
     func nextRound() {
-        if questionsAsked == questionsPerRound {
+        // Checking equality here can lead to an endless loop of questions.
+        if questionsAsked >= questionsPerRound {
             // Game is over
             displayScore()
         } else {
@@ -132,7 +143,7 @@ class ViewController: UIViewController {
     }
     
     // MARK: Helper Methods
-    
+    // Bunch of repetition here. Polish this up.
     func hideChoices() {
         let choices = [choiceA,choiceB,choiceC,choiceD]
         for button in choices {
@@ -144,6 +155,15 @@ class ViewController: UIViewController {
         let choices = [choiceA,choiceB,choiceC,choiceD]
         for button in choices {
             button.hidden = false
+        }
+    }
+    
+    func resetButtons() {
+        let choices = [choiceA,choiceB,choiceC,choiceD]
+        
+        for button in choices {
+            button.enabled = true
+            button.backgroundColor = UIColor.init(red: 12.0/255.0, green: 121/255.0, blue: 150/255.0, alpha: 1.0) // the default blue button
         }
     }
     // little less verbose than the contained function
@@ -163,6 +183,18 @@ class ViewController: UIViewController {
         }
     }
     
+    func highlightAnswer() {
+        let choices = [choiceA,choiceB,choiceC,choiceD]
+        for button in choices {
+            // set buttons to disabled so you can't tap multiple times
+            button.enabled = false
+            if button.titleLabel?.text == trivia.questions[indexOfSelectedQuestion].correctAnswer {
+                button.backgroundColor = UIColor.init(red: 22.0/255.0, green: 136.0/255.0, blue: 28/255.0, alpha: 1.0) // a nice shade of green
+            } else {
+                button.backgroundColor = UIColor.init(red: 245.0/255.0, green: 56.0/255.0, blue: 74/255.0, alpha: 1.0) // a nice shade of red
+            }
+        }
+    }
     
 }
 
